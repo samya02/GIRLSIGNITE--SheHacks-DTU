0f8284d7b7b96c548258bca268dcb8374caf6fa8
Kashika Akhouri <kashika0112@gmail.com>
2021-04-11 11:15:29 +0530

    Front-end for blog page added

--- scripts1.py
+++ scripts1.py
@@ -3,6 +3,12 @@ from flask import Flask, render_template
 
 app=Flask(__name__)
 
+def __init__(self, name,email,college,ideas):
+    self.name=name
+    self.email=email
+    self.college=college
+    self.ideas=ideas
+
 @app.route('/')
 def home():
     return render_template("home.html")
@@ -20,9 +26,14 @@ def blog():
 def contact():
     return render_template("contact.html")
 
-@app.route('/success/')
+@app.route('/success/', methods=['POST'])
 def success():
+    
     return render_template("success.html")
+
+@app.route('/stories/')
+def stories():
+    return render_template("stories.html")
 
 if __name__=="__main__":
     app.run(debug=True)
